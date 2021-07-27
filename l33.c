c4754bd97b2411872aceefa3944ed71cb214cc20
cn <cool_navy@qq.com>
2021-07-27 23:45:25 +0800

    up 33

--- l33.c
+++ l33.c
@@ -3,6 +3,13 @@
 #include <math.h>
 #include <string.h>
 
+/*
+整数数组 nums 按升序排列，数组中的值 互不相同 。
+
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 int binsearch(int* nums, int target, int i, int j){
     if(i < 0 || j < 0 || j < i) return -1;
     int h = (i+j)/2;
@@ -33,7 +40,6 @@ int roundSearch(int* nums, int target, int i, int j){
     }
 }
 
-// https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
 int rsearch(int* nums, int numsSize, int target){
     return roundSearch(nums, target, 0, numsSize-1);
 }
