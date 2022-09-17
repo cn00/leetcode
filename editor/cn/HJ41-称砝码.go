f52fa97195691c5d27023fd225627ea284907c78
cn <cool_navy@qq.com>
2022-09-17 19:13:52 +0800

    up HJ 41 单维位标记法

--- editor/cn/HJ41-称砝码.go
+++ editor/cn/HJ41-称砝码.go
@@ -27,15 +27,19 @@ func NewBitFlag(l uint) BitFlag {
 	return flg
 }
 
-func (b *BitFlag) Get(i uint) bool {
-	return b.buf[i/8]&(0x1<<i%8) != 0
+func (f *BitFlag) Get(i uint) (b bool) {
+	b = f.buf[i/8]&(0x1<<(i%8)) != 0
+	return
 }
 
-func (b *BitFlag) Set(i uint) {
-	b.buf[i/8] |= byte(0x01 << (i % 8))
+func (f *BitFlag) Set(i uint) {
+	f.buf[i/8] |= byte(0x01 << (i % 8))
 }
-func (b *BitFlag) Count() (c uint) {
-	for _, i := range b.buf {
+func (f *BitFlag) Unset(i uint) {
+	f.buf[i/8] &= byte((0x01 << (i % 8)) ^ 0xff)
+}
+func (f *BitFlag) Count() (c uint) {
+	for _, i := range f.buf {
 		for j := 0; j < 8; j++ {
 			if i&(0x01<<j) > 0 {
 				c++
@@ -45,15 +49,15 @@ func (b *BitFlag) Count() (c uint) {
 	return
 }
 
-func (b *BitFlag) BinStr() (s string) {
-	for i := len(b.buf) - 1; i >= 0; i-- {
-		s += fmt.Sprintf("%08b ", b.buf[i])
+func (f *BitFlag) BinStr() (s string) {
+	for i := len(f.buf) - 1; i >= 0; i-- {
+		s += fmt.Sprintf("%08b ", f.buf[i])
 	}
 	return
 }
 
-func (b *BitFlag) Print(s ...any) {
-	fmt.Println(b.BinStr())
+func (f *BitFlag) Print() {
+	fmt.Println(f.BinStr())
 }
 
 func main() {
@@ -86,85 +90,22 @@ func main() {
 		}
 	}
 	//fmt.Println("w2", w2)
-	var dp = make([][]bool, len(w2)+1)
-	var dp2 = NewBitFlag(uint(sum + 1))
-	//dp2.Print()
-	for i := 0; i <= len(w2); i++ {
-		dp[i] = make([]bool, uint(sum+1))
-		dp[i][0] = true
-	}
-	for i := 1; i < len(w2)+1; i++ {
-		for j := uint(1); j < sum+1; j++ {
-			if j >= w2[i-1] {
-				dp[i][j] = dp[i-1][j] || dp[i-1][j-w2[i-1]]
-			} else {
-				dp[i][j] = dp[i-1][j]
+	var dp = NewBitFlag(sum + 1)
+	//dp.Print()
+
+	dp.Set(0)
+	sum2 := uint(0)
+	for i := uint(0); i < uint(len(w2)); i++ {
+		sum2 += w2[i]
+		for j := sum2; j > w2[i]; j-- {
+			if dp.Get(j - w2[i]) {
+				dp.Set(j)
+				//fmt.Println(i,j, w2[i],":",dp.BinStr())
 			}
-			if dp[i][j] {
-				dp2.Set(uint(j))
-			}
-
-			//fmt.Println(i, j, dp2.BinStr(), ">", dp[i][j])
 		}
+		dp.Set(w2[i])
+		//fmt.Println(i,w2[i],":",dp.BinStr())
 	}
-	fmt.Println(dp2.Count() + 1)
-}
-
-func main2() {
-	fmt.Println("称砝码")
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Split(bufio.ScanWords)
-	sc.Scan()
-	n, _ := strconv.Atoi(sc.Text())
-	w := []int{}
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		m, _ := strconv.Atoi(sc.Text())
-		w = append(w, m)
-	}
-
-	sum := 0
-	m := []int{}
-	for i := 0; i < n; i++ {
-		sc.Scan()
-		x, _ := strconv.Atoi(sc.Text())
-		m = append(m, x)
-		sum += w[i] * x
-	}
-	fmt.Println("m", m)
-
-	var w2 = []int{}
-	for i := 0; i < n; i++ {
-		for j := 0; j < m[i]; j++ {
-			w2 = append(w2, w[i])
-		}
-	}
-	fmt.Println("w2", w2)
-	var dp = make([]int, sum+1)
-	//dp[0] = 1
-	for i := 0; i < len(w2); i++ {
-		dp[w2[i]] = w2[i]
-		fmt.Println(i, dp)
-		for j := 1; j < sum+1; j++ {
-			if j >= w2[i] {
-				dp[j] = max(dp[j], dp[j-w2[i]]+w2[i])
-			}
-
-			fmt.Println(i, j, dp)
-		}
-	}
-	c := 0
-	for j, v := range dp {
-		if v == j {
-			c++
-		}
-	}
-	fmt.Println(c)
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
-	}
+	//fmt.Println(dp.BinStr())
+	fmt.Println(dp.Count())
 }
